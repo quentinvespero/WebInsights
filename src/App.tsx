e987ea337d7e120c3ebebefe6cc09280630d1a99
Quentin Vespero <quentinvespero@gmail.com>
2024-10-09 01:19:33 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 const App = () => {
+
+    const style = {
+        container: {
+            // width: "100%",
+            // height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        }
+    }
+
     return (
-        <div>
+        <div style={style.container}>
             <h1>Web Insights</h1>
         </div>
     )
