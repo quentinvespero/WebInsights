cc19cb36501890e83744d8cab7a6cbb5c72fba11
Quentin Vespero <quentinvespero@gmail.com>
2024-10-09 02:16:41 +0200

    added style to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ const App = () => {
 
     const style = {
         container: {
-            // width: "100%",
-            // height: "100%",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
